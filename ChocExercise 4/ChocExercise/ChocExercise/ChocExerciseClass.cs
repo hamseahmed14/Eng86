b71063264acc9bb6b73571d83315119f66b32bda
Hamse Ahmed <hamse2727@gmail.com>
2021-04-21 16:44:31 +0100

    Add ChocExercise

---
+++ ChocExercise 4/ChocExercise/ChocExercise/ChocExerciseClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChocExercise
+{
+	public class ChocExercise
+	{
+        public static int BreakChocolate(int n, int m)
+        {
+            if (n == 0 || m == 0) 
+                return 0;
+            else
+                return (n * m) - 1;
+        }
+    }
+}
