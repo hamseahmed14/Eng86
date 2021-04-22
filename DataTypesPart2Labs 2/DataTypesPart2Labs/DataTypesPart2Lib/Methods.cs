063e324ca29546e3c9c1d65a9f87c09d71b003ee
Hamse Ahmed <hamse2727@gmail.com>
2021-04-22 18:31:05 +0100

    Add Data Types exercise

---
+++ DataTypesPart2Labs 2/DataTypesPart2Labs/DataTypesPart2Lib/Methods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace DataTypesPt2Lib
+{
+    public enum Suit
+    {
+        HEARTS, CLUBS, DIAMONDS, SPADES
+    }
+    public class Methods
+    {
+        public static int AgeAt(DateTime birthDate, DateTime date)
+        {
+            if (birthDate > date)
+            {
+                throw  new ArgumentException("Error - birthDate is in the future");
+            }
+            var day = date - birthDate;
+            var age = day.Days/365.25;
+            return (int)age;
+        }
+
+        public static string FormatDate(DateTime date)
+        {
+            return date.ToString("y/dd/MMM");
+        }
+        public static string GetMonthString(DateTime date)
+        {
+            return date.ToString("MMMM");
+        }
+
+        public static string Fortune(Suit suit)
+        {
+            switch (suit)
+            {
+                case Suit.HEARTS:
+                    return "You've broken my heart";
+                    break;
+                case Suit.CLUBS:
+                    return "And the seventh rule is if this is your first night at fight club, you have to fight.";
+                    break;
+                case Suit.DIAMONDS:
+                    return "Diamonds are a girls best friend";
+                    break;
+                case Suit.SPADES:
+                    return "Bucket and spade";
+                    break;
+                default:
+                    break;
+            }
+            return string.Empty;
+        }
+
+
+    }
+}
+    
